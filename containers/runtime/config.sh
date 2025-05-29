f32bfa8fc44bef055576686acfffd1369ff83b56
Marina Gonokhina <marina.gonokhina@fulldive.com>
2025-05-29 19:47:18 +0300

    tru with this configs

--- containers/runtime/config.sh
+++ containers/runtime/config.sh
@@ -5,3 +5,6 @@ DOCKER_IMAGE=runtime
 # These variables will be appended by the runtime_build.py script
 # DOCKER_IMAGE_TAG=
 # DOCKER_IMAGE_SOURCE_TAG=
+
+DOCKER_IMAGE_TAG=oh_v0.39.2_image_nikolaik_s_python-nodejs_tag_python3.12-nodejs22
+DOCKER_IMAGE_SOURCE_TAG=oh_v0.39.2_21dw5cow8uicajef_1rcl1ehmx1jy5y3z
